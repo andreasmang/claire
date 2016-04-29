4c000ad461acc4d061d4746a6e72dda95914e5c8
Andreas Mang <andreas@ices.utexas.edu>
2016-04-29 14:56:50 -0500

    build script improved

--- external/build_libs.sh
+++ external/build_libs.sh
@@ -10,6 +10,12 @@ enableavx=0		# enable AVX	# (Advanced Vector Extensions (AVX)
 								# set architecture for microprocessors
 								# from Intel and AMD)
 
+buildfftw=0
+buildaccfft=0
+buildpnetcdf=0
+buildnifticlib=0
+buildpetsc=0
+cleanup=0
 for i in "$@"
 do
 case $i in
@@ -22,6 +28,37 @@ case $i in
     ;;
     --build)
     builddep=1
+    ;;
+    --bfftw)
+    buildfftw=1
+    ;;
+    --baccfft)
+    buildaccfft=1
+    ;;
+    --bpnetcdf)
+    buildpnetcdf=1
+    ;;
+    --bnifti)
+    buildnifticlib=1
+    ;;
+    --bnifti)
+    buildpetsc=1
+    ;;
+    --clean)
+    cleanup=1
+    ;;
+    --help)
+    echo "script to build dependencies (libraries)" 
+    echo "  1) libraries are: FFTW; ACCFFT; PNETCDF; NIFTICLIB; PETSc;"
+    echo "  2) options for this script are"
+    echo "     --help         print this message"
+    echo "     --build        build all libraries"
+    echo "     --bpnetcdf     build PNETCDF library"
+    echo "     --bfftw        build FFTW library"
+    echo "     --baccfft      build ACCFFT library (depends on FFTW & PNETCDF)"
+    echo "     --bnifti       build NIFTI library"
+    echo "     --bpetsc       build PETSc library"
+    echo "     --clean        remove all libraries (deletes all subfolders)"
     ;;
     *)
     # unknown option
@@ -74,9 +111,13 @@ if [ ! -d ${FFTW_LIB_DIR} ]; then
 	mkdir ${FFTW_LIB_DIR}/build
 	echo extracting FFTW lib...
 	tar -xzf ${LIB_DIR}/fftw-3.3.4.tar.gz -C ${SRC_DIR} --strip-components=1
+else
+	if [ ${cleanup} -eq 1 -a ! ${FFTW_LIB_DIR} == ${HOME} ]; then
+		rm -rf ${FFTW_LIB_DIR}
+	fi
 fi
 
-if [ ${builddep} -eq 1 ]; then 
+if [ ${builddep} -eq 1 -o ${buildfftw} -eq 1 ]; then 
 	echo "configuring FFTW (double precision)" 
 	cd ${SRC_DIR}
 	if [ ${enableavx} -eq 1 ]; then
@@ -116,10 +157,14 @@ if [ ! -d ${PNETCDF_LIB_DIR} ]; then
 	mkdir ${SRC_DIR}
 	echo extracting PNETCDF lib...
 	tar -xzf ${LIB_DIR}/parallel-netcdf-1.7.0.tar.gz -C ${SRC_DIR} --strip-components=1
+else
+	if [ ${cleanup} -eq 1 -a ! ${PNETCDF_LIB_DIR} == ${HOME} ]; then
+		rm -rf ${PNETCDF_LIB_DIR}
+	fi
 fi
 
 
-if [ ${builddep} -eq 1 ]; then 
+if [ ${builddep} -eq 1 -o ${buildpnetcdf} -eq 1 ]; then 
 	echo "configuring PNETCDF" 
 	cd ${SRC_DIR}
 	./configure --prefix=${BLD_DIR} --with-mpi=${MPI_DIR} MPICC=${MPI_C} MPICXX=${MPI_CXX} CXXFLAGS='-O3' CPPFLAGS='-O3' --disable-fortran #FFLAGS='-O3' FCFLAGS='-O3'  
@@ -148,10 +193,15 @@ if [ ! -d ${ACCFFT_LIB_DIR} ]; then
 	mkdir ${CMK_DIR}
 	echo extracting ACCFFT lib...
 	tar -xzf ${LIB_DIR}/accfft.tar.gz -C ${SRC_DIR} --strip-components=1
+else
+	if [  ${cleanup} -eq 1  -a  ! ${ACCFFT_LIB_DIR} == ${HOME}  ]; then
+		rm -rf ${ACCFFT_LIB_DIR}
+	fi
 fi
 
 
-if [ ${builddep} -eq 1 ]; then 
+if [ ${builddep} -eq 1 -o ${buildaccfft} -eq 1 ]; then 
+    
 	echo "configuring ACCFFT" 
 	cd ${CMK_DIR}
 	cmake ${SRC_DIR} -DCMAKE_INSTALL_PREFIX=${BLD_DIR} -DFFTW_ROOT=${FFTW_LIB_DIR}/build -DFFTW_USE_STATIC_LIBS=true -DBUILD_GPU=false -DBUILD_STEPS=true -DCXX_FLAGS="-O3" -DBUILD_SHARED=false -DPNETCDF_DIR=${PNETCDF_LIB_DIR}/build
@@ -180,13 +230,17 @@ if [ ! -d ${PETSC_LIB_DIR} ]; then
 fi
 
 PETSC_ARCH=cxx_opt
-if [ ${builddep} -eq 1 ]; then 
+if [ ${builddep} -eq 1 -o ${buildpetsc} -eq 1 ]; then 
 	echo "configuring PETSC" 
 	cd ${SRC_DIR}
 	./configure PETSC_DIR=${SRC_DIR} PETSC_ARCH=${PETSC_ARCH} --prefix=${BLD_DIR} --with-clanguage=C++ --with-debugging=0 --download-f2cblaslapack --with-shared=1 --with-x=0 --with-64-bit-indices --with-c++-support=1 --with-pthread=1 COPTFLAGS='-O3' CXXOPTFLAGS='-O3' --with-cc=${MPI_C} --with-cxx=${MPI_CXX} --with-fc=0
 	echo "building PETSC" 
 	make PETSC_DIR=${SRC_DIR} PETSC_ARCH=${PETSC_ARCH}
 	make install
+else
+	if [ ${cleanup} -eq 1 -a ! ${PETSC_LIB_DIR} == ${HOME} ]; then
+		rm -rf ${PETSC_LIB_DIR}
+	fi
 fi
 
 echo "export PETSC_DIR=${BLD_DIR}" >> ${BUILD_DIR}/environment_vars.sh
@@ -210,9 +264,13 @@ if [ ! -d ${NIFTI_LIB_DIR} ]; then
 	mkdir ${SRC_DIR}
 	echo extracting NIFTI lib...
 	tar -xzf ${LIB_DIR}/nifticlib-2.0.0.tar.gz -C ${SRC_DIR} --strip-components=1
+else
+	if [ ${cleanup} -eq 1 -a ! ${NIFTI_LIB_DIR} == "~/" ]; then
+		rm -rf ${NIFTI_LIB_DIR}
+	fi
 fi
 
-if [ ${builddep} -eq 1 ]; then 
+if [ ${builddep} -eq 1 -o ${buildnifticlib} -eq 1 ]; then 
 	echo "configuring NIFTICLIB" 
 	cd ${SRC_DIR}
 	cmake ${SRC_DIR} -DBUILD_SHARED_LIBS:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=${BLD_DIR} -Wno-dev
