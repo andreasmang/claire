db3ebfa25427724bb94f233d5d7804dda9ec40ea
Andreas Mang <andreas@ices.utexas.edu>
2016-10-27 18:12:41 -0500

    user can now set type of regularization norm (for real)

--- include/RegOpt.hpp
+++ include/RegOpt.hpp
@@ -404,7 +404,7 @@ class RegOpt {
 
     // regularization
     inline RegNorm GetRegNorm() {return this->m_RegNorm;}
-    inline void SetRegNorm(RegNorm flag) {this->m_RegNorm = flag;}
+    inline void SetRegNormType(RegNormType flag) {this->m_RegNorm.type = flag;}
     inline void SetRegularizationWeight(int i, ScalarType beta) {
         this->m_RegNorm.beta[i] = beta;
     }
