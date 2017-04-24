2eaa0695a2f1d4cd179d8c3d4d4f5a32144a87b3
Andreas Mang <andreas@ices.utexas.edu>
2017-04-24 16:06:51 -0500

    fixed bash file to download libraries

--- external/get_libs.sh
+++ external/get_libs.sh
@@ -1,37 +1,51 @@
 #!/bin/bash
 
-echo "----------------------------------------------------------------------------------"
-echo "downloading ACCFFT"
-echo "----------------------------------------------------------------------------------"
-wget http://users.ices.utexas.edu/~andreas/libs/accfft.tar.gz .
+if [[ ! -e accfft.tar.gz ]]; then
+	echo "----------------------------------------------------------------------------------"
+	echo "downloading accFFT"
+	echo "----------------------------------------------------------------------------------"
+	wget http://users.ices.utexas.edu/~andreas/libs/accfft.tar.gz .
+fi
 
-echo "----------------------------------------------------------------------------------"
-echo "downloading FFTW 3.3.6 PL1"
-echo "----------------------------------------------------------------------------------"
-wget http://users.ices.utexas.edu/~andreas/libs/fftw-3.3.6-pl1.tar.gz .
+if [[ ! -e fftw-3.3.6-pl1.tar.gz ]]; then
+	echo "----------------------------------------------------------------------------------"
+	echo "downloading FFTW 3.3.6 PL1"
+	echo "----------------------------------------------------------------------------------"
+	wget http://users.ices.utexas.edu/~andreas/libs/fftw-3.3.6-pl1.tar.gz .
+fi
 
 
-echo "----------------------------------------------------------------------------------"
-echo "downloading morton library"
-echo "----------------------------------------------------------------------------------"
-wget http://users.ices.utexas.edu/~andreas/libs/libmorton.tar.gz
+if [[ ! -e libmorton.tar.gz ]]; then
+	echo "----------------------------------------------------------------------------------"
+	echo "downloading morton library"
+	echo "----------------------------------------------------------------------------------"
+	wget http://users.ices.utexas.edu/~andreas/libs/libmorton.tar.gz
+fi
 
-echo "----------------------------------------------------------------------------------"
-echo "downloading nifti library 2.0.0"
-echo "----------------------------------------------------------------------------------"
-wget http://users.ices.utexas.edu/~andreas/libs/nifticlib-2.0.0.tar.gz
+if [[ ! -e nifticlib-2.0.0.tar.gz ]]; then
+	echo "----------------------------------------------------------------------------------"
+	echo "downloading nifti library 2.0.0"
+	echo "----------------------------------------------------------------------------------"
+	wget http://users.ices.utexas.edu/~andreas/libs/nifticlib-2.0.0.tar.gz
+fi
 
-echo "----------------------------------------------------------------------------------"
-echo "downloading netcdf library 1.7.0"
-echo "----------------------------------------------------------------------------------"
-wget http://users.ices.utexas.edu/~andreas/libs/parallel-netcdf-1.7.0.tar.gz
+if [[ ! -e parallel-netcdf-1.7.0.tar.gz ]]; then
+	echo "----------------------------------------------------------------------------------"
+	echo "downloading netcdf library 1.7.0"
+	echo "----------------------------------------------------------------------------------"
+	wget http://users.ices.utexas.edu/~andreas/libs/parallel-netcdf-1.7.0.tar.gz
+fi
 
-echo "----------------------------------------------------------------------------------"
-echo "downloading petsc library 3.7.3"
-echo "----------------------------------------------------------------------------------"
-wget http://users.ices.utexas.edu/~andreas/libs/petsc-lite-3.7.3.tar.gz
+if [[ ! -e petsc-lite-3.7.3.tar.gz ]]; then
+	echo "----------------------------------------------------------------------------------"
+	echo "downloading petsc library 3.7.3"
+	echo "----------------------------------------------------------------------------------"
+	wget http://users.ices.utexas.edu/~andreas/libs/petsc-lite-3.7.3.tar.gz
+fi
 
-echo "----------------------------------------------------------------------------------"
-echo "downloading zlib 1.2.8"
-echo "----------------------------------------------------------------------------------"
-wget http://users.ices.utexas.edu/~andreas/libs/zlib-1.2.8.tar.gz
+if [[ ! -e zlib-1.2.8.tar.gz ]]; then
+	echo "----------------------------------------------------------------------------------"
+	echo "downloading zlib 1.2.8"
+	echo "----------------------------------------------------------------------------------"
+	wget http://users.ices.utexas.edu/~andreas/libs/zlib-1.2.8.tar.gz
+fi
