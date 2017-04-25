d8d81fbfb91e9d8f778aa357f3bdb0ad4aeb694d
Andreas Mang <andreas@ices.utexas.edu>
2017-04-25 09:59:06 -0500

    script for libraries updated (library details are now hidden to user)

--- external/get_libs.sh
+++ external/get_libs.sh
@@ -1,51 +1,58 @@
 #!/bin/bash
 
 if [[ ! -e accfft.tar.gz ]]; then
+	echo ""
 	echo "----------------------------------------------------------------------------------"
 	echo "downloading accFFT"
 	echo "----------------------------------------------------------------------------------"
 	wget http://users.ices.utexas.edu/~andreas/libs/accfft.tar.gz .
 fi
 
-if [[ ! -e fftw-3.3.6-pl1.tar.gz ]]; then
+if [[ ! -e fftw.tar.gz ]]; then
+	echo ""
 	echo "----------------------------------------------------------------------------------"
-	echo "downloading FFTW 3.3.6 PL1"
+	echo "downloading FFTW"
 	echo "----------------------------------------------------------------------------------"
-	wget http://users.ices.utexas.edu/~andreas/libs/fftw-3.3.6-pl1.tar.gz .
+	wget http://users.ices.utexas.edu/~andreas/libs/fftw.tar.gz .
 fi
 
 
 if [[ ! -e morton.tar.gz ]]; then
+	echo ""
 	echo "----------------------------------------------------------------------------------"
 	echo "downloading morton library"
 	echo "----------------------------------------------------------------------------------"
 	wget http://users.ices.utexas.edu/~andreas/libs/morton.tar.gz
 fi
 
-if [[ ! -e nifticlib-2.0.0.tar.gz ]]; then
+if [[ ! -e nifticlib.tar.gz ]]; then
+	echo ""
 	echo "----------------------------------------------------------------------------------"
-	echo "downloading nifti library 2.0.0"
+	echo "downloading nifti library"
 	echo "----------------------------------------------------------------------------------"
-	wget http://users.ices.utexas.edu/~andreas/libs/nifticlib-2.0.0.tar.gz
+	wget http://users.ices.utexas.edu/~andreas/libs/nifticlib.tar.gz
 fi
 
-if [[ ! -e parallel-netcdf-1.7.0.tar.gz ]]; then
+if [[ ! -e parallel-netcdf.tar.gz ]]; then
+	echo ""
 	echo "----------------------------------------------------------------------------------"
-	echo "downloading netcdf library 1.7.0"
+	echo "downloading netcdf library"
 	echo "----------------------------------------------------------------------------------"
-	wget http://users.ices.utexas.edu/~andreas/libs/parallel-netcdf-1.7.0.tar.gz
+	wget http://users.ices.utexas.edu/~andreas/libs/parallel-netcdf.tar.gz
 fi
 
-if [[ ! -e petsc-lite-3.7.3.tar.gz ]]; then
+if [[ ! -e petsc-lite.tar.gz ]]; then
+	echo ""
 	echo "----------------------------------------------------------------------------------"
-	echo "downloading petsc library 3.7.3"
+	echo "downloading petsc library"
 	echo "----------------------------------------------------------------------------------"
-	wget http://users.ices.utexas.edu/~andreas/libs/petsc-lite-3.7.3.tar.gz
+	wget http://users.ices.utexas.edu/~andreas/libs/petsc.tar.gz
 fi
 
-if [[ ! -e zlib-1.2.8.tar.gz ]]; then
+if [[ ! -e zlib.tar.gz ]]; then
+	echo ""
 	echo "----------------------------------------------------------------------------------"
-	echo "downloading zlib 1.2.8"
+	echo "downloading zlib"
 	echo "----------------------------------------------------------------------------------"
-	wget http://users.ices.utexas.edu/~andreas/libs/zlib-1.2.8.tar.gz
+	wget http://users.ices.utexas.edu/~andreas/libs/zlib.tar.gz
 fi
