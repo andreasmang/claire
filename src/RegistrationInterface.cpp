9c93c105546e148cf140f6f9658dcfa80bdb6d12
Andreas Mang <andreas@ices.utexas.edu>
2017-05-18 10:25:38 -0500

    residual computation fixed

--- src/RegistrationInterface.cpp
+++ src/RegistrationInterface.cpp
@@ -316,37 +316,51 @@ PetscErrorCode RegistrationInterface::GetFinalState(Vec m1) {
  *******************************************************************/
 PetscErrorCode RegistrationInterface::GetResidual(Vec residual) {
     PetscErrorCode ierr = 0;
-    Vec lambda = NULL;
-    IntType nl, nc, nt, l0, l1;
-    ScalarType *p_l = NULL, *p_res = NULL;
+    Vec lambda = NULL, m = NULL;
+    IntType nl, nc, nt, l0, l1, l;
+    ScalarType *p_l = NULL, *p_res = NULL, *p_m = NULL, *p_mr = NULL;
     PetscFunctionBegin;
 
     ierr = Assert(residual != NULL, "null pointer"); CHKERRQ(ierr);
     ierr = Assert(this->m_RegProblem != NULL, "null pointer"); CHKERRQ(ierr);
 
-    // get lambda
-    ierr = this->m_RegProblem->GetAdjointVariable(lambda); CHKERRQ(ierr);
 
     nc = this->m_Opt->GetDomainPara().nc;
     nl = this->m_Opt->GetDomainPara().nl;
     nt = this->m_Opt->GetDomainPara().nt;
 
-    ierr = VecGetArray(lambda, &p_l); CHKERRQ(ierr);
+    // get array for residual
     ierr = VecGetArray(residual, &p_res); CHKERRQ(ierr);
 
     if (this->m_Opt->GetOptPara().method == FULLNEWTON) {
+        // get lambda
+        ierr = this->m_RegProblem->GetAdjointVariable(lambda); CHKERRQ(ierr);
+        ierr = VecGetArray(lambda, &p_l); CHKERRQ(ierr);
         l0 =  nt*nc*nl; l1 = (nt+1)*nc*nl;
+        try {std::copy(p_l+l0, p_l+l1, p_res);}
+        catch (std::exception&) {
+            ierr = ThrowError("copy failed"); CHKERRQ(ierr);
+        }
+        ierr = VecRestoreArray(lambda, &p_l); CHKERRQ(ierr);
     } else {
-        l0 = 0; l1 = nc*nl;
+        l = nt*nc*nl;  // index for final condition
+        // compute terminal condition \lambda_1 = -(m_1 - m_R) = m_R - m_1
+        ierr = this->m_RegProblem->GetStateVariable(m); CHKERRQ(ierr);
+        ierr = VecGetArray(m, &p_m); CHKERRQ(ierr);
+        ierr = VecGetArray(this->m_ReferenceImage, &p_mr); CHKERRQ(ierr);
+#pragma omp parallel
+{
+#pragma omp for
+        for (IntType i = 0; i < nc*nl; ++i) {
+            p_l[i] = p_mr[i] - p_m[l+i];  // compute initial condition
+        }
+}  // omp
+        ierr = VecRestoreArray(this->m_ReferenceImage, &p_mr); CHKERRQ(ierr);
+        ierr = VecRestoreArray(m, &p_m); CHKERRQ(ierr);
     }
 
-    try {std::copy(p_l+l0, p_l+l1, p_res);}
-    catch (std::exception&) {
-        ierr = ThrowError("copy failed"); CHKERRQ(ierr);
-    }
 
     ierr = VecRestoreArray(residual, &p_res); CHKERRQ(ierr);
-    ierr = VecRestoreArray(lambda, &p_l); CHKERRQ(ierr);
 
     PetscFunctionReturn(ierr);
 }
