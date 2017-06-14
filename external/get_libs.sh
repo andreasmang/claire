ffada8e10a497df53f150e6ecdef79a4bb2ac8c6
Andreas Mang <andreas@ices.utexas.edu>
2017-06-14 11:14:47 -0500

    download for libraries now points to the actual urls (at least for some of them)

--- external/get_libs.sh
+++ external/get_libs.sh
@@ -5,7 +5,9 @@ if [[ ! -e accfft.tar.gz ]]; then
 	echo "----------------------------------------------------------------------------------"
 	echo "downloading accFFT"
 	echo "----------------------------------------------------------------------------------"
-	wget http://users.ices.utexas.edu/~andreas/libs/accfft.tar.gz .
+	wget http://users.ices.utexas.edu/~andreas/libs/accfft.tar.gz
+#	wget -O accfft-master.zip https://github.com/amirgholami/accfft/archive/master.zip
+#	tar -czvf master.tar.gz master
 fi
 
 if [[ ! -e fftw.tar.gz ]]; then
@@ -13,7 +15,8 @@ if [[ ! -e fftw.tar.gz ]]; then
 	echo "----------------------------------------------------------------------------------"
 	echo "downloading FFTW"
 	echo "----------------------------------------------------------------------------------"
-	wget http://users.ices.utexas.edu/~andreas/libs/fftw.tar.gz .
+	wget -O fftw.tar.gz http://www.fftw.org/fftw-3.3.6-pl2.tar.gz
+#	wget http://users.ices.utexas.edu/~andreas/libs/fftw.tar.gz .
 fi
 
 
@@ -30,6 +33,7 @@ if [[ ! -e nifticlib.tar.gz ]]; then
 	echo "----------------------------------------------------------------------------------"
 	echo "downloading nifti library"
 	echo "----------------------------------------------------------------------------------"
+	#wget -O nifticlib.tar.gz --no-check-certificate http://downloads.sourceforge.net/project/niftilib/nifticlib/nifticlib_2_0_0/nifticlib-2.0.0.tar.gz
 	wget http://users.ices.utexas.edu/~andreas/libs/nifticlib.tar.gz
 fi
 
@@ -38,7 +42,8 @@ if [[ ! -e parallel-netcdf.tar.gz ]]; then
 	echo "----------------------------------------------------------------------------------"
 	echo "downloading netcdf library"
 	echo "----------------------------------------------------------------------------------"
-	wget http://users.ices.utexas.edu/~andreas/libs/parallel-netcdf.tar.gz
+	wget -O parallel-netcdf.tar.gz http://cucis.ece.northwestern.edu/projects/PnetCDF/Release/parallel-netcdf-1.8.1.tar.gz 
+	#wget http://users.ices.utexas.edu/~andreas/libs/parallel-netcdf.tar.gz
 fi
 
 if [[ ! -e petsc-lite.tar.gz ]]; then
@@ -46,7 +51,8 @@ if [[ ! -e petsc-lite.tar.gz ]]; then
 	echo "----------------------------------------------------------------------------------"
 	echo "downloading petsc library"
 	echo "----------------------------------------------------------------------------------"
-	wget http://users.ices.utexas.edu/~andreas/libs/petsc.tar.gz
+	wget -O petsc-lite.tar.gz http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.7.6.tar.gz
+#	wget http://users.ices.utexas.edu/~andreas/libs/petsc.tar.gz
 fi
 
 if [[ ! -e zlib.tar.gz ]]; then
