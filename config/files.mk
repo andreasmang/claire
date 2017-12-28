a3d9f0f558918f84b6e07bd1d7552ad889616aeb
Andreas Mang <andreas@math.uh.edu>
2017-12-28 13:35:47 -0600

    files removed from make file

---
+++ config/files.mk
@@ -0,0 +1,32 @@
+CPPFILES=$(SRCDIR)/RegOpt.cpp \
+		$(SRCDIR)/RegToolsOpt.cpp \
+		$(SRCDIR)/CLPBenchmark.cpp \
+		$(SRCDIR)/RegUtils.cpp \
+		$(SRCDIR)/ghost.cpp \
+		$(SRCDIR)/interp3.cpp \
+		$(SRCDIR)/Interp3_Plan.cpp \
+		$(SRCDIR)/VecField.cpp \
+		$(SRCDIR)/TenField.cpp \
+		$(SRCDIR)/ReadWriteReg.cpp \
+		$(SRCDIR)/SynProbRegistration.cpp \
+		$(SRCDIR)/SemiLagrangian.cpp \
+		$(SRCDIR)/Optimizer.cpp \
+		$(SRCDIR)/KrylovInterfaceReg.cpp \
+		$(SRCDIR)/TaoInterfaceRegistration.cpp \
+		$(SRCDIR)/RegistrationInterface.cpp \
+		$(SRCDIR)/MultiLevelPyramid.cpp \
+		$(SRCDIR)/Preconditioner.cpp \
+		$(SRCDIR)/RegularizationRegistration.cpp \
+		$(SRCDIR)/RegularizationRegistrationL2.cpp \
+		$(SRCDIR)/RegularizationRegistrationH1.cpp \
+		$(SRCDIR)/RegularizationRegistrationH2.cpp \
+		$(SRCDIR)/RegularizationRegistrationH1SN.cpp \
+		$(SRCDIR)/RegularizationRegistrationH2SN.cpp \
+		$(SRCDIR)/RegularizationRegistrationH3.cpp \
+		$(SRCDIR)/RegularizationRegistrationH3SN.cpp \
+		$(SRCDIR)/OptimizationProblem.cpp \
+		$(SRCDIR)/OptimalControlRegistrationBase.cpp \
+		$(SRCDIR)/OptimalControlRegistration.cpp \
+		$(SRCDIR)/OptimalControlRegistrationIC.cpp \
+		$(SRCDIR)/OptimalControlRegistrationRelaxedIC.cpp \
+		$(SRCDIR)/Preprocessing.cpp
