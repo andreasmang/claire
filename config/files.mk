188ee86d12bcbfbdbb189195accd47ddcabe5f4c
brunnme <malte.brunn@ipvs.uni-stuttgart.de>
2018-08-29 17:01:43 +0200

    reorganizing file structure

--- config/files.mk
+++ config/files.mk
@@ -43,21 +43,21 @@ CPPFILES=$(SRCDIR)/RegOpt.cpp \
 		$(SRCDIR)/CLAIREDivReg.cpp \
 		$(SRCDIR)/Preprocessing.cpp 
 
-CUFILES=$(SRCDIR)/interp3_gpu_new.cu \
-		$(SRCDIR)/distance_kernel.cu \
+CUFILES=$(SRCDIR)/Interpolation/interp3_gpu_new.cu \
+		$(SRCDIR)/DistanceMeasure/distance_kernel.cu \
 		$(SRCDIR)/adjoint_kernel.cu \
-		$(SRCDIR)/RegularizationKernel.cu
+		$(SRCDIR)/Regularization/RegularizationKernel.cu
 
 CPPFILESCUDA=$(SRCDIR)/RegOpt.cpp \
 		$(SRCDIR)/RegToolsOpt.cpp \
 		$(SRCDIR)/BenchmarkOpt.cpp \
 		$(SRCDIR)/CLAIREUtils.cpp \
 		$(SRCDIR)/ghost.cpp \
-		$(SRCDIR)/interp3.cpp \
-		$(SRCDIR)/Interp3_Plan.cpp \
-		$(SRCDIR)/Differentiation.cpp \
-		$(SRCDIR)/DifferentiationFD.cpp \
-		$(SRCDIR)/DifferentiationSM.cpp \
+		$(SRCDIR)/Interpolation/interp3.cpp \
+		$(SRCDIR)/Interpolation/Interp3_Plan.cpp \
+		$(SRCDIR)/Differentiation/Differentiation.cpp \
+		$(SRCDIR)/Differentiation/DifferentiationFD.cpp \
+		$(SRCDIR)/Differentiation/DifferentiationSM.cpp \
 		$(SRCDIR)/VecField.cpp \
 		$(SRCDIR)/TenField.cpp \
 		$(SRCDIR)/ReadWriteReg.cpp \
@@ -66,29 +66,29 @@ CPPFILESCUDA=$(SRCDIR)/RegOpt.cpp \
 		$(SRCDIR)/TransportEquation.cpp \
 		$(SRCDIR)/ContinuityEquation.cpp \
 		$(SRCDIR)/DeformationFields.cpp \
-		$(SRCDIR)/DistanceMeasure.cpp \
-		$(SRCDIR)/DistanceMeasureNCC.cpp \
-		$(SRCDIR)/DistanceMeasureSL2.cpp \
-		$(SRCDIR)/DistanceMeasureSL2aux.cpp \
-		$(SRCDIR)/SemiLagrangian.cpp \
+		$(SRCDIR)/DistanceMeasure/DistanceMeasure.cpp \
+		$(SRCDIR)/DistanceMeasure/DistanceMeasureNCC.cpp \
+		$(SRCDIR)/DistanceMeasure/DistanceMeasureSL2.cpp \
+		$(SRCDIR)/DistanceMeasure/DistanceMeasureSL2aux.cpp \
+		$(SRCDIR)/SemiLagrangian/SemiLagrangian.cpp \
+		$(SRCDIR)/SemiLagrangian/SemiLagrangianGPUNew.cpp \
 		$(SRCDIR)/Optimizer.cpp \
 		$(SRCDIR)/KrylovInterface.cpp \
 		$(SRCDIR)/TaoInterface.cpp \
 		$(SRCDIR)/CLAIREInterface.cpp \
 		$(SRCDIR)/MultiLevelPyramid.cpp \
 		$(SRCDIR)/Preconditioner.cpp \
-		$(SRCDIR)/Regularization.cpp \
-		$(SRCDIR)/RegularizationL2.cpp \
-		$(SRCDIR)/RegularizationH1.cpp \
-		$(SRCDIR)/RegularizationH2.cpp \
-		$(SRCDIR)/RegularizationH1SN.cpp \
-		$(SRCDIR)/RegularizationH2SN.cpp \
-		$(SRCDIR)/RegularizationH3.cpp \
-		$(SRCDIR)/RegularizationH3SN.cpp \
+		$(SRCDIR)/Regularization/Regularization.cpp \
+		$(SRCDIR)/Regularization/RegularizationL2.cpp \
+		$(SRCDIR)/Regularization/RegularizationH1.cpp \
+		$(SRCDIR)/Regularization/RegularizationH2.cpp \
+		$(SRCDIR)/Regularization/RegularizationH1SN.cpp \
+		$(SRCDIR)/Regularization/RegularizationH2SN.cpp \
+		$(SRCDIR)/Regularization/RegularizationH3.cpp \
+		$(SRCDIR)/Regularization/RegularizationH3SN.cpp \
 		$(SRCDIR)/OptimizationProblem.cpp \
 		$(SRCDIR)/CLAIREBase.cpp \
 		$(SRCDIR)/CLAIRE.cpp \
 		$(SRCDIR)/CLAIREStokes.cpp \
 		$(SRCDIR)/CLAIREDivReg.cpp \
-		$(SRCDIR)/Preprocessing.cpp \
-		$(SRCDIR)/SemiLagrangianGPUNew.cpp
+		$(SRCDIR)/Preprocessing.cpp
--- include/RegularizationGPU.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __REGULARIZATIONGPU_HPP__
-#define __REGULARIZATIONGPU_HPP__
-
-#include "typedef.hpp"
-
-void EvaluateGradientH1SN_GPU(ComplexType *v1hat, ComplexType *v2hat, ComplexType *v3hat, IntType ostart[3], IntType nl[3], IntType nx[3], ScalarType opfact);
-
-#endif
