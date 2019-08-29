7b70d623c8a16f4d77a7d73b8e9939928fa7ef94
naveenaero <naveen01.iitb@gmail.com>
2019-08-29 12:22:53 -0500

    change betavmin for h1 and h2

--- include/RegOpt.hpp
+++ include/RegOpt.hpp
@@ -334,10 +334,10 @@ struct KrylovMethod {
 
 /* parameter for parameter continuation (regularization parameter) */
 struct ParCont {
-//    static constexpr ScalarType betavminh1 = 1E-4;    ///< minimal regularization parameter for h1 type norm
-//    static constexpr ScalarType betavminh2 = 1E-7;    ///< minimal regularization parameter for h2 type norm
-    static constexpr ScalarType betavminh1 = 1E-9;      ///< minimal regularization parameter for h1 type norm
-    static constexpr ScalarType betavminh2 = 1E-9;      ///< minimal regularization parameter for h2 type norm
+    static constexpr ScalarType betavminh1 = 1E-3;    ///< minimal regularization parameter for h1 type norm
+    static constexpr ScalarType betavminh2 = 1E-7;    ///< minimal regularization parameter for h2 type norm
+    //static constexpr ScalarType betavminh1 = 1E-9;      ///< minimal regularization parameter for h1 type norm
+    //static constexpr ScalarType betavminh2 = 1E-9;      ///< minimal regularization parameter for h2 type norm
     static constexpr ScalarType betascale = 1E-1;       ///< default reduction factor (one order of magnitude)
     static constexpr ScalarType dbetascale = 1E-2;      ///< default reduction factor (one order of magnitude)
     static const int maxsteps = 10;                     ///< max number of steps
@@ -346,6 +346,7 @@ struct ParCont {
     ScalarType targetbeta;                              ///< target regularization parameter
     ScalarType beta0;                                   ///< initial regularization parameter
     ScalarType stepsize;
+    ScalarType betamin;
 };
 
 
