18ff8a9ac6b2f1b4679a0ade3e10b7d38c28605b
Naveen Himthani <naveen15@login1.maverick2.tacc.utexas.edu>
2019-10-01 21:41:54 -0500

    ComputeDisplacementField() fixed; betavmin changed

--- include/RegOpt.hpp
+++ include/RegOpt.hpp
@@ -341,10 +341,10 @@ struct KrylovMethod {
 
 /* parameter for parameter continuation (regularization parameter) */
 struct ParCont {
-//    static constexpr ScalarType betavminh1 = 1E-4;    ///< minimal regularization parameter for h1 type norm
-//    static constexpr ScalarType betavminh2 = 1E-7;    ///< minimal regularization parameter for h2 type norm
-    static constexpr ScalarType betavminh1 = 1E-9;      ///< minimal regularization parameter for h1 type norm
-    static constexpr ScalarType betavminh2 = 1E-9;      ///< minimal regularization parameter for h2 type norm
+    static constexpr ScalarType betavminh1 = 1E-4;    ///< minimal regularization parameter for h1 type norm
+    static constexpr ScalarType betavminh2 = 1E-7;    ///< minimal regularization parameter for h2 type norm
+//    static constexpr ScalarType betavminh1 = 1E-9;      ///< minimal regularization parameter for h1 type norm
+//    static constexpr ScalarType betavminh2 = 1E-9;      ///< minimal regularization parameter for h2 type norm
     static constexpr ScalarType betascale = 1E-1;       ///< default reduction factor (one order of magnitude)
     static constexpr ScalarType dbetascale = 1E-2;      ///< default reduction factor (one order of magnitude)
     static const int maxsteps = 10;                     ///< max number of steps
